45cab4047561949f703625c48d9cb360ab39f076
LPATROCINIO1969 <lpatrocinio@terra.com.br>
2021-11-03 14:43:00 -0300

    Criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -9,6 +9,41 @@ public class PrimeiroPrograma {
         System.out.println("Hello, World! " + (a+b));*/
         Gato gato = new Gato();
         System.out.println(gato);
+        Livro livro = new Livro("Triste Fim de Policarpo Quaresma",250);
+        System.out.println(livro);
+    }
+}
 
+class Livro{
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
